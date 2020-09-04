18bba5ea6cc1c008eefd6031acf7ba651a983932
Thunderbolt2812 <Gustavopineda95@gmail.com>
2020-09-04 13:24:15 -0600

    Clases creadas

--- Lab6_GustavoPineda/src/lab6_gustavopineda/Canciones.java
+++ Lab6_GustavoPineda/src/lab6_gustavopineda/Canciones.java
@@ -10,5 +10,67 @@ package lab6_gustavopineda;
  * @author Usuario
  */
 public class Canciones {
+    private String titulo;
+    private int puntuacion;
+    private String año;
+    private String artista;
+    private String album;
+
+    public Canciones() {
+    }
+
+    public Canciones(String titulo, int puntuacion, String año, String artista, String album) {
+        this.titulo = titulo;
+        this.puntuacion = puntuacion;
+        this.año = año;
+        this.artista = artista;
+        this.album = album;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
+    
     
 }
--- Lab6_GustavoPineda/src/lab6_gustavopineda/Playlist.java
+++ Lab6_GustavoPineda/src/lab6_gustavopineda/Playlist.java
@@ -5,10 +5,44 @@
  */
 package lab6_gustavopineda;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Usuario
  */
 public class Playlist {
+    private String nombre;
+    private ArrayList<Canciones> canciones;
+
+    public Playlist() {
+    }
+
+    public Playlist(String nombre, ArrayList<Canciones> canciones) {
+        this.nombre = nombre;
+        this.canciones = canciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Canciones> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(ArrayList<Canciones> canciones) {
+        this.canciones = canciones;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
     
 }
